b3405492616c5ef9b738276550252465b9f93a76
bharat-rajani <bharat.ramrajani@gmail.com>
2019-07-30 15:23:55 +0530

    :art: Formatting code

--- src/main/java/com/bharat/polls/security/CustomUserDetailsService.java
+++ src/main/java/com/bharat/polls/security/CustomUserDetailsService.java
@@ -19,13 +19,15 @@ public class CustomUserDetailsService implements UserDetailsService {
     @Override
     @Transactional
     public UserDetails loadUserByUsername(String usernameOrEmail) throws UsernameNotFoundException {
-            User user = userRepository.findByUsernameOrEmail(usernameOrEmail, usernameOrEmail).orElseThrow(() -> new UsernameNotFoundException("User not found with username or email: "+usernameOrEmail));
+            User user = userRepository.findByUsernameOrEmail(usernameOrEmail, usernameOrEmail)
+                    .orElseThrow(() -> new UsernameNotFoundException("User not found with username or email: "+usernameOrEmail));
         return UserPrincipal.create(user);
     }
 
     @Transactional
     public UserDetails loadUserById(Long id) throws UsernameNotFoundException{
-        User user = userRepository.findById(id).orElseThrow(()->new UsernameNotFoundException("User not found with id: "+id));
+        User user = userRepository.findById(id)
+                .orElseThrow(()->new UsernameNotFoundException("User not found with id: "+id));
         return UserPrincipal.create(user);
     }
 
